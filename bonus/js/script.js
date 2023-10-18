4859c9bea1edf0c2b6cf79ef2121ae0821b35326
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-18 15:57:11 +0200

    scaffolding-bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,33 @@
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click',
+function()
+{
+    const squareContainer = document.getElementById('grid_container');
+
+    // generare n numeri elementi
+    for(let i = 1; i <= 100; i ++)
+    {
+        let square = getSquare(i);
+        squareContainer.append(square)
+    }
+    
+});
+
+function getSquare(squareIndex)
+{
+    // creare elementi e applicare lo sitle
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.innerHTML = squareIndex;
+
+    // cambio sfondo al click
+    square.addEventListener('click',
+    function()
+    {
+        square.classList.add('active');
+        console.log(squareIndex)
+    });
+    return square;
+};
