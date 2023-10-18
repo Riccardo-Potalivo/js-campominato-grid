14ea2b2b6316d15c2cc533be1658f613d0a657de
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-18 16:41:30 +0200

    grid-element-select-set

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -8,20 +8,24 @@ function()
     // reset griglia al click
     squareContainer.innerHTML = '';
     // generare n numeri elementi
-    for(let i = 1; i <= 100; i ++)
+    let squareNumber = 100;
+    for(let i = 1; i <= squareNumber; i ++)
     {
-        let square = getSquare(i);
+        let square = getSquare(i, squareNumber);
         squareContainer.append(square)
     }
     
 });
 
-function getSquare(squareIndex)
+function getSquare(squareIndex, squareNumber)
 {
     // creare elementi e applicare lo sitle
     const square = document.createElement('div');
+    let squareHeight = Math.sqrt(squareNumber)
+
     square.classList.add('square');
     square.innerHTML = squareIndex;
+    square.style.height = `calc(100% / ${squareHeight})`
 
     // cambio sfondo al click
     square.addEventListener('click',
