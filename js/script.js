c3e5e5980f66122105ab85f505d0adaf995d150d
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-18 15:35:42 +0200

    square-numbers-1-100

--- js/script.js
+++ js/script.js
@@ -7,18 +7,19 @@ function()
     const squareContainer = document.getElementById('grid_container');
 
     // generare n numeri elementi
-    for(let i = 1; i <= 35; i ++)
+    for(let i = 1; i <= 100; i ++)
     {
-        let square = getSquare();
+        let square = getSquare(i);
         squareContainer.append(square)
     }
     
 });
 
-function getSquare()
+function getSquare(squareIndex)
 {
     // creare elementi e applicare lo sitle
     const square = document.createElement('div');
     square.classList.add('square');
+    square.innerHTML = squareIndex;
     return square;
 };
